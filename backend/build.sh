9a2e7651db1f42b8f52aed6e4daf79ba9746d9fd
John Mark <wanjugujohnmark@gmail.com>
2025-08-14 15:56:40 +0300

    other updates24

--- backend/build.sh
+++ backend/build.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 # build.sh
 
-apt-get update && apt-get install -y \
-    tesseract-ocr \
-    libtesseract-dev
-
-# Verify Tesseract installation
+apt-get update
+apt-get install -y tesseract-ocr libtesseract-dev
+export PATH=$PATH:/usr/bin
+#verify installation
 tesseract --version || { echo "Tesseract installation failed or not found!"; exit 1; }
 
 # Install Python dependencies
