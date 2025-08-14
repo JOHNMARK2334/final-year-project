beb7e381a604d06dff19435100f6e28e8cc86995
John Mark <wanjugujohnmark@gmail.com>
2025-08-14 14:19:30 +0300

    other updates

--- backend/database.py
+++ backend/database.py
@@ -7,7 +7,6 @@ def init_db(app):
     import os
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
         'DATABASE_URL',
-        'postgresql://medical_assistant1_user:CqhjQalOFodFEUKuTZrQJDUnaXgAMU8D@dpg-d2ep9pbuibrs738978eg-a.oregon-postgres.render.com/medical_assistant1'
     )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
