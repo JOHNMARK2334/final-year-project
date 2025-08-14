c24074bd6160b9262358ae08ca2f7040b48b4a2b
John Mark <wanjugujohnmark@gmail.com>
2025-08-14 16:10:06 +0300

    other updates26

--- backend/build.sh
+++ backend/build.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 # build.sh
-
-#!/bin/bash
-pip install pytesseract pillow
-pip install -r requirements.txt
-#verify installation
-tesseract --version || { echo "Tesseract installation failed or not found!"; exit 1; }
-
 # Install Python dependencies
 pip install -r requirements.txt
 
